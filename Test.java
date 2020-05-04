293b78949f56507a6b5f89e447b27c74e6bc756e
mmubeen <adilfazal8486@gmail.com>
2020-05-04 07:48:59 +0530

    updated readme, added test.main

---
+++ Test.java
@@ -0,0 +1,11 @@
+//this is an example of project to use of git on ubuntu
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for testing");
+		System.out.println("Github repository updated!");
+
+	}
+
+}
